3ab1cb554556c34c57e779db24064a88b180df4e
lipskissm <lipskissm0@gmail.com>
2024-03-15 14:29:14 +0000

    added a hero and its css to look good, removed usless scrollbar

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import Lipskis from '../Assets/Lipskis.png'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-top">
+        <img src={Lipskis} alt=""/>
+    </div>
+
+    <div className="hero-down">
+
+    </div>
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/Pages/Ontap.jsx
+++ frontend/src/Pages/Ontap.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Hero from '../Components/Hero/Hero'
 const Ontap = () => {
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
