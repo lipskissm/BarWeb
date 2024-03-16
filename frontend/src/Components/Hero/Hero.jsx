b4870cecc1f4d554da5c8c8ca93c128f4ec1025b
lipskissm <lipskissm0@gmail.com>
2024-03-16 12:42:46 +0000

    added a selection which is not responsive

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Hero.css'
 import Lipskis from '../Assets/Lipskis.png'
+import gubex from '../Assets/gubextr_lip.png'
+import gubtms from '../Assets/gubtams_lip.png'
+import mara from '../Assets/mara.png'
+import ipa from '../Assets/ipa.png'
+import gun from '../Assets/guiness.png'
+import hoe from '../Assets/hoe.png'
+import wachen from '../Assets/wachen.png'
 const Hero = () => {
   return (
     <div className='hero'>
@@ -11,6 +18,15 @@ const Hero = () => {
     <div className="text">
       <p style={{fontWeight: 'bold'}}>Šiuo metu:</p>
 
+    </div>
+    <div className='lipdukai-btn'>
+      <li><img src={gubex} alt=''/></li>
+      <li><img src={gubtms} alt=''/></li>
+      <li><img src={mara} alt=''/></li>
+      <li><img src={ipa} alt=''/></li>
+      <li><img src={gun} alt=''/></li>
+      <li><img src={hoe} alt=''/></li>
+      <li><img src={wachen} alt=''/></li>
     </div>
     </div>
   )
