9cfe54f499d632b820b69e33194590e88cf2067a
lipskissm <lipskissm0@gmail.com>
2024-03-22 14:17:03 +0000

    added login properties, lazy to style them now

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/Login.css'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='Login'>
+      <div className="Login-container">
+        <h1>Registruotis</h1>
+        <div className="LoginSignup-fields">
+          <input type="text" placeholder='Vardas'/>
+          <input type="email" placeholder='Elektroninis pastas'/>
+          <input type="password" placeholder='Slaptazodis'/>
+        </div>
+        <button>Continue</button>
+        <p className="login-login">Jau vienas is musu? <span>Prisijuk</span></p>
+        <div className="login-agree">
+          <input type = "checkbox" name='' id='' />
+
+        </div>
+      </div>
     </div>
   )
 }
