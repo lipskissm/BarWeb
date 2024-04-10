f4c9d619c3bae011d8b87c79bfb96ca06d190410
lipskissm <lipskissm0@gmail.com>
2024-04-10 15:37:50 +0100

    commit

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -19,20 +19,36 @@ const CartItems = () => {
      {all_product.map((e)=>{
         if(cartItems[e.id]>0){
             return <div>
-            <div className="cartitems-format">
+            <div className="cartitems-format cartitem-format-main">
                 <img src={e.image} alt="" className='carticon-product-icon' />
                 <p>{e.name}</p>
                 <p>${e.price}</p>
                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
                 <p>{e.price * cartItems[e.id]}</p>
-                <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                <img className='remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
             </div>
             <hr />
          </div>
         }
+        return null;
      })}
-    </div>
+     <div className="cartitems-down">
+      <div className="cartitems-total">
+         <h1>Cart Total</h1>
+         <div>
+           <div className="caritems-total-item">
+            <p>Total</p>
+            <p>${0}</p>
+           </div> 
+           <hr/>
+           </div>
+         </div>
+         <button>Testi</button>
+      </div>
+     </div>
+    
   )
+
 }
 
 export default CartItems
