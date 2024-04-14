58a1ebe81ec6214beca3f738660dea4ca182eefd
lipskissm <lipskissm0@gmail.com>
2024-04-14 12:10:04 +0100

    running the server

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,27 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//db con mongo 
+mongoose.connect("mongodb+srv://lipskissm0:LipskioAline2!@cluster0.kcoptqk.mongodb.net/Lipskis");
+
+// API creation
+
+app.get("/", (req, res)=>{
+    res.send("Express is running");
+
+})
+
+
+app.listen(port,(error)=>{
+    if(!error){
+       console.log("Server running on Port"+port) 
+    }
+    else
+    {
+        console.log("Error : "+error)
+    }
+})
