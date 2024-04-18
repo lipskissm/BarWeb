3a23a46c9e405e5be996d214096b6518c974de58
lipskissm <lipskissm0@gmail.com>
2024-04-18 15:39:55 +0100

    nesakysiu

--- backend/index.js
+++ backend/index.js
@@ -75,9 +75,18 @@ const Product = mongoose.model("Product",{
 })
 
 app.post('/addproduct',async (req,res)=> {
-
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id = 1;
+    }
     const product = new Product({
-        id:req.body.id,
+        id:id,
         name:req.body.name,
         image:req.body.image,
         category:req.body.category,
@@ -88,6 +97,10 @@ app.post('/addproduct',async (req,res)=> {
     console.log(product);
     await product.save();
     console.log("Saved")
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
 })
 
     
