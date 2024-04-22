af3c823da197c68e1c44acd0a7a3ceeae6290810
lipskissm <lipskissm0@gmail.com>
2024-04-22 18:53:17 +0100

    ryt pirma darbo diena

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddProduct.css'
+import upload from '../../assets/upload.png'
 const AddProduct = () => {
+    const[image,setImage] = useState(false);
+    const[productDetails, setProductDetails] = useState({
+        name:"",
+        image:"",
+        category:"Alus",
+        price:""
+
+    })
+    const imageHandler = (e)=>{
+        setImage(e.target.files[0]);
+    }
+
+    const changeHandler = (e)=>{
+        setProductDetails({...productDetails,[e.target.name]:e.target.value})
+    }
+
+    const Add_Product = async ()=>{
+        console.log(productDetails);
+        let responseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                Accept:'application/json'
+            },
+            body:formData,
+        }).then((resp) =>resp.json()).then((data)=>{responseData=data})
+   
+        if(responseData.success){
+            product.image=responseData.image_url;
+            console.log(product);
+        }
+    }
+
+
   return (
     <div className='add-product'>
       <div className="addproduct-itemfield">
-        <p>Product title</p>
-        <input type="text" name='name' placeholder='Type Here'/>
+        <p>Produkto pavadinimas</p>
+        <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Tekstas'/>
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfield">
+            <p>Kaina</p>
+            <input value={productDetails.price} onChange={changeHandler} type="text" name='price' placeholder='Tekstas'>   
+            </input>
+        </div>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Produkto kategorija</p>
+        <select value={productDetails.category} onChange={changeHandler} name="category" className='addproduct-selector'>
+            <option value="Alus">Alus</option>
+            <option value="Sidras">Sidras</option>
+            <option value="Nealkoholiniai">Nealkoholiniai</option>
+        </select>
+        <div className="addproduct-itemfield">
+            <label htmlFor="file-input">
+                <img src={image?URL.createObjectURL(image):upload} className='addproduct-tumbnail-img' alt="" />
+            </label>
+            <input onChange={imageHandler} type='file' name="image" id="file-input" hidden/>
+            <button onClick={()=>{Add_Product()}} className='addproduct-btn'>Prideti</button>
+        </div>
       </div>
     </div>
   )
