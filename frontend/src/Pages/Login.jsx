c58803cbf0c16c89da739a51495178e9b5dec3bc
lipskissm <lipskissm0@gmail.com>
2024-05-12 13:25:02 +0100

    added states for login and sign up

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Login.css'
 
 const Login = () => {
+
+const [state,setState] = useState("Registracija");
+
+
   return (
     <div className='Login'>
       <div className="Login-container">
-        <h1>Registruotis</h1>
+        <h1>{state}</h1>
         <div className="LoginSignup-fields">
-          <input type="text" placeholder='Vardas'/>
+          {state === "Registracija"?<input type="text" placeholder='Vardas'/>:<></>}
           <input type="email" placeholder='Elektroninis paštas'/>
           <input type="password" placeholder='Slaptažodis'/>
         </div>
         <button>Tęsti</button>
-        <p className="login-login">Jau vienas iš mūsų? <span>Prisijunk</span></p>
+        {state === "Registracija"?<p className="login-login">Jau vienas iš mūsų? <span onClick={()=>{setState("Prisijunk")}}>Prisijunk</span></p>:<></>}
+        {state === "Prisijunk"?<p className="login-login">Neturi prisijungimo? <span onClick={()=>{setState("Registracija")}}>Prisiregistruok</span></p>:<></>}
         <div className="login-agree">
           <input type = "checkbox" name='' id='' />
           <p> Tęsdami sutinkate su privatumo politika </p>
