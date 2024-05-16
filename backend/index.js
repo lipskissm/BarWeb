7da5c59944cae49f093cdb0a39d1eb452aa267b8
lipskissm <lipskissm0@gmail.com>
2024-05-16 13:05:31 +0100

    complete

--- backend/index.js
+++ backend/index.js
@@ -201,10 +201,50 @@ app.post('/login',async (req,res)=>{
     }
 })
 
+// middleware to fetch user 
+
+const fetchUser = async(req, res,next)=>{
+    const token = req.header('auth-token');
+    if (!token){
+        res.status(401).send({errors:"Prasau prisijungti su vartotoju"})
+    }
+    else {
+        try {
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        } catch(error){
+            res.status(401).send({errors:"Prisijunkite su esamu vartotoju"})
+        }
+    }
+}
+
+
 //endpoint ot add cart in db
-app.post('/addtocart', async (res,req)=>{
-    console.log(req.body)
+app.post('/addtocart',fetchUser, async (req,res)=>{
+    let userData = await Users.findOne({_id:req.user.id})
+    userData.cartData[req.body.itemId] +=1
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Pridetas")
 })
+
+// endpoint to remove items from cart
+app.post('/removefromcart', fetchUser, async(req,res)=>{
+    let userData = await Users.findOne({_id:req.user.id})
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -=1
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Nuomtas")
+})
+
+// apri for cart data
+app.post('/getcart', fetchUser, async(req,res)=>{
+    console.log("GetCart");
+    let userData = await Users.findOne({_id:req.user.id})
+    res.json(userData.cartData)
+    
+})
+
 
 app.listen(port,(error)=>{
     if(!error){
